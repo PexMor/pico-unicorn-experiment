1aecf498b42c286ddddca1a852a6d2f6f0117701
PexMor <p.moravek@xlinux.cz>
2021-04-30 10:46:35 +0200

    describe the tools

---
+++ desktop-py/README.md
@@ -0,0 +1,9 @@
+# The Desktop utilities
+
+This folder is intended to be run in desktop Python3.
+
+The script helps to convert standard images into byte buffer for use in Micro Python.
+
+When run it also creates the preview as html table (save.html). As well as jpg preview.
+
+The main output for copy and paste experiments is sent to stdout.
--- desktop-py/imgCvt.py
+++ desktop-py/imgCvt.py
@@ -26,15 +26,22 @@ np_im = numpy.array(im3)
 print(np_im.shape)
 amax, bmax, rgb = np_im.shape
 buf = bytearray()
+html = "<html><head><title>ColorMatrix</title></head><body><table>"
 for a in range(amax):
+    html += "<tr>"
     for b in range(bmax):
+        hex_rgb = "%02x%02x%02x" % (np_im[a][b][0],np_im[a][b][1],np_im[a][b][2])
+        html += "<td style='background-color:#"+hex_rgb+"'>@</td>"
         for c in range(rgb):
-            if a<4 and b<4 and c == 0:
-                val = 255
-            else:
-                val = 0
+            #if a<4 and b<4 and c == 0:
+            #    val = 255
+            #else:
+            #    val = 0
             val = np_im[a][b][c]
             buf.append(val)
             # buf.append(np_im[a][b][c])
-
+    html += "</tr>";
+html += "</body></html>"
+with open("save.html", "w") as tf:
+    tf.write(html)
 print(buf)
